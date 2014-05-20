05d808e8bc790903adaff43fb307380efa7cf584
cowhi <github.rg@gmail.com>
2014-05-20 13:25:22 -0300

    improved functions in part2

--- week1/part2.rb
+++ week1/part2.rb
@@ -4,13 +4,17 @@ end
 
 
 def starts_with_consonant?(s)
-  no_consonant = ["a", "e", "i", "o", "u"]
-  not no_consonant.include?(s[0,1].downcase) 
+  if s.size > 0
+    no_consonant = ["a", "e", "i", "o", "u"]
+    return !(no_consonant.include?(s[0,1].downcase)) 
+  else 
+    return false
+  end
 end
 
 
 def binary_multiple_of_4?(s)
-  if /^[0,1]*$/ === s
+  if /^[0,1]*$/ === s and s.size > 0
     if s.to_i(base=2) % 4 == 0
       return true
     end
@@ -24,7 +28,11 @@ puts hello("Ruben")
 
 puts starts_with_consonant?("Ruben")
 puts starts_with_consonant?("Aksel")
+puts starts_with_consonant?("")
+puts starts_with_consonant?("#foo")
 
 puts binary_multiple_of_4?("10101010101")
 puts binary_multiple_of_4?("100")
-puts binary_multiple_of_4?("001")
+puts binary_multiple_of_4?("abcd")
+puts binary_multiple_of_4?("")
+puts binary_multiple_of_4?("#foo")
