ba7d4c9f78850dbe44465025ff894d90cab81a09
cowhi <github.rg@gmail.com>
2014-05-20 10:50:09 -0300

    improved sum_to_n?

--- week1/part1.rb
+++ week1/part1.rb
@@ -15,12 +15,13 @@ def max_2_sum(integer_array)
 end
 
 def sum_to_n?(integer_array, integer_n)
-  if integer_array.size == 0 || 1
+  if integer_array.size == 0 or integer_array.size == 1
     return false
   end
   integer_array.each_with_index { |value1, index1|
     integer_array.each_index{  |value2, index2|
       if index1 != index2
+        #puts "#{value1} + #{value2} = %d", value1 + value2
         if value1 + value2 == integer_n
           return true
         end
@@ -30,5 +31,5 @@ def sum_to_n?(integer_array, integer_n)
   return false
 end
 
-print sum_to_n?([1],1)
-
+#puts sum_to_n?([1],1)
+#puts sum_to_n?([1,2,3,4,5], 5)
